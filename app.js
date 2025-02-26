8d4ea320349d73239c6e1cf6903339d83d13c77c
João Victor Andrade <55860973+Jandrade2k@users.noreply.github.com>
2025-02-26 10:11:26 -0300

    Remove comments
    removi os comentários do app.js
    
    
    estrutura basica pronta só falta fazer funcionar

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Rotas (exemplo)
 app.get('/', (req, res) => {
   res.send('API está rodando!');
 });
 
-// Importe suas rotas aqui
-// const usuarioRoutes = require('./routes/usuarioRoutes');
-// app.use('/usuarios', usuarioRoutes);
-
-// Inicie o servidor
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`);
 });
